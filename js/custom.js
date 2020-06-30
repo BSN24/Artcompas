898fcb2e6eb3592a3723d5661968d55743c4f931
bsn24 <prizrak24@gmail.com>
2020-06-30 20:51:47 +0600

    Верстка выпадающего меню

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
 	$('#soa-property-1').attr("placeholder", "Иванов Иван Иванович");
 	$('#soa-property-2').attr("placeholder", "email@email.ru");
 	$('#soa-property-3').attr("placeholder", "_(___)___-__-__");
-	$("#soa-property-3").inputmask("9(999)999-99-99")
+	$("#soa-property-3").inputmask("9(999)999-99-99");
+
+    $('.menu-desktop__btn').click(function (e) {
+        e.preventDefault();
+
+        var parentItem = $(this).parents('.menu-desktop__main').siblings('.menu-desktop__submenu');
+
+        $(this).toggleClass('menu-desktop__btn--open');
+
+        parentItem.eq(0).slideToggle(250);
+    })
 });
